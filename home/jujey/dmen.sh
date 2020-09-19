6aeb3276bd6eee489e45f71ad615c2d57a3df96e
jujé <58881699+jujey@users.noreply.github.com>
2020-09-19 01:58:34 +0100

    Update dmen.sh

--- home/jujey/dmen.sh
+++ home/jujey/dmen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -b -fn "Terminus-9" -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
+dmenu_run -b -fn "Terminus-11" -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"

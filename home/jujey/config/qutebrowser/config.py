603f569937a88c101bde78b36ae5c2b24eb686dd
jujé <58881699+jujey@users.noreply.github.com>
2020-10-03 13:43:47 +0100

    Add files via upload

--- home/jujey/config/qutebrowser/config.py
+++ home/jujey/config/qutebrowser/config.py
@@ -3,7 +3,7 @@ config.load_autoconfig()
 config.set("colors.webpage.darkmode.enabled", True)
 
 c.tabs.indicator.width = 0
-c.tabs.padding = {'top': 4, 'bottom': 4, 'left': 4, 'right': 4}
+c.tabs.padding = {'top': 10, 'bottom': 10, 'left': 10, 'right': 10}
 c.tabs.favicons.show = 'never'
 c.tabs.title.format = '{current_title}'
 

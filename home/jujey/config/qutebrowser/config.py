6f8f92f63668b9a3d71c5b8f8516bfe7fc5a8566
jujé <58881699+jujey@users.noreply.github.com>
2021-01-07 19:32:27 +0000

    Add files via upload

--- home/jujey/config/qutebrowser/config.py
+++ home/jujey/config/qutebrowser/config.py
@@ -3,19 +3,21 @@ config.load_autoconfig()
 config.set("colors.webpage.darkmode.enabled", True)
 
 c.tabs.indicator.width = 0
-c.tabs.padding = {'top': 5, 'bottom': 5, 'left': 5, 'right': 5}
+c.tabs.padding = {'top': 8, 'bottom': 8, 'left': 8, 'right': 8}
 c.tabs.favicons.show = 'never'
 c.tabs.title.format = '{current_title}'
+c.tabs.title.alignment = 'center'
+c.tabs.padding = 2
 
-c.fonts.tabs.selected = '11pt Terminus'
-c.fonts.tabs.unselected = '11pt Terminus'
-c.fonts.hints = '11pt Terminus'
-c.fonts.keyhint = '11pt Terminus'
-c.fonts.prompts = '11pt Terminus'
-c.fonts.downloads = '11pt Terminus'
-c.fonts.statusbar = '11pt Terminus'
-c.fonts.contextmenu = '11pt Terminus'
-c.fonts.messages.info = '11pt Terminus'
-c.fonts.debug_console = '11pt Terminus'
-c.fonts.completion.entry = '11pt Terminus'
-c.fonts.completion.category = '11pt Terminus'
+c.fonts.tabs.selected = '12pt Terminus'
+c.fonts.tabs.unselected = '12pt Terminus'
+c.fonts.hints = '12pt Terminus'
+c.fonts.keyhint = '12pt Terminus'
+c.fonts.prompts = '12pt Terminus'
+c.fonts.downloads = '12pt Terminus'
+c.fonts.statusbar = '12pt Terminus'
+c.fonts.contextmenu = '12pt Terminus'
+c.fonts.messages.info = '12pt Terminus'
+c.fonts.debug_console = '12pt Terminus'
+c.fonts.completion.entry = '12pt Terminus'
+c.fonts.completion.category = '12pt Terminus'
